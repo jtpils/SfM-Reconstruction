2ad03ae66fecc2697b6847fe337ce9f7ef44f83e
Charic Daniel Farinango Cuervo <charicfc@gmail.com>
2019-12-03 17:34:17 -0600

    saving and reading matches from files

--- features.py
+++ features.py
@@ -23,9 +23,8 @@ class View():
 		self.keypoints = []
 		self.descriptors = []
 		self.method = feat_det_type
-		self.projection_matrix = np.zeros((3, 4), dtype=np.float32)
 		self.rotation_matrix = np.zeros((3, 3), dtype=np.float32)
-		self.translation_matrix = np.zeros((3, 1), dtype=np.float32)
+		self.translation_vector = np.zeros((3, 1), dtype=np.float32)
 		self.matched_points = []
 		self.matches = []
 		self.indices = []
@@ -91,6 +90,29 @@ class View():
 			descriptors.append(temp_descriptor)
 		return keypoints, np.array(descriptors)
 
+	def write_matches_file(self, previous_view_name):
+
+		temp_array = []
+		for idx, match in enumerate(self.matches):
+			temp = (match.distance, match.imgIdx, match.queryIdx, match.trainIdx)
+			temp_array.append(temp)
+		
+		matches_file = open('matches/'+ previous_view_name + '_' + self.file_name + '.pkl', 'wb')
+		pickle.dump(temp_array, matches_file) 
+		matches_file.close()
+
+	def read_matches_file(self, filename):
+
+		matches = []
+
+		file_matches = pickle.load( open(filename, "rb" ) )
+
+		for point in file_matches:
+			temp_feature = cv2.DMatch(_distance=point[0], _imgIdx=point[1], _queryIdx=point[2], _trainIdx=point[3])
+			matches.append(temp_feature)
+
+		self.matches = matches
+
 	def extract_features(self, features_path):
 
 		# Resizing step
@@ -121,100 +143,128 @@ class View():
 
 
 def get_files_paths(folder_path):
+
+
 	files_paths = []
 
-	# r=root, d=directories, f = files
-	for r, d, f in os.walk(folder_path):
-		for file in f:
-			if '.jpg' in file or '.pkl':
-				files_paths.append(os.path.join(r, file))
+	if folder_path != None:
+		# r=root, d=directories, f = files
+		for r, d, f in os.walk(folder_path):
+			for file in f:
+				if '.jpg' in file or '.pkl':
+					files_paths.append(os.path.join(r, file))
 
-	return files_paths
+	if len(files_paths) == 0:
+		return None
+
+	else:
+		return files_paths
+
 
 # returns two values: result to plot matches, list with coordinates of matches
 def match(view1, view2, matcher_alg='brute_force', distance_type=''):
 
-	# Brute Force Matching
+	matches_paths = get_files_paths('matches/')
 
-	# Params: First one is normType. It specifies the distance measurement to be used. By default, it is cv.NORM_L2. It is good for SIFT, SURF etc 
-	# (cv.NORM_L1 is also there). For binary string based descriptors like ORB, BRIEF, BRISK etc, cv.NORM_HAMMING should be used, which used
-	# Hamming distance as measurement. If ORB is using WTA_K == 3 or 4, cv.NORM_HAMMING2 should be used.
-	# Second param is boolean variable, crossCheck which is false by default. If it is true, Matcher returns only those matches with value (i,j) 
-	# such that i-th descriptor in set A has j-th descriptor in set B as the best match and vice-versa. That is, the two features in both sets 
-	# should match each other.
+	filename = 'matches/'+ view1.file_name + '_' + view2.file_name + '.pkl'
 
-	matched_points = []
-	closest_matches = []
-	crossCheck = False if distance_type == 'ratio' else True
+	if matches_paths != None and os.path.isfile(filename):
 
-	if feature_detection == 'sift' or feature_detection == 'surf':
-
-		if matcher_alg == 'brute_force':
-			matcher = cv2.BFMatcher(cv2.NORM_L2, crossCheck=crossCheck)
-
-		elif matcher_alg == 'flann': 
-			FLANN_INDEX_KDTREE = 1
-			index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-			search_params = dict(checks=50)   # or pass empty dictionary
-
-			matcher = cv2.FlannBasedMatcher(index_params,search_params)
-		
-	elif feature_detection == 'orb':
-
-		if matcher_alg == 'brute_force':
-			matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=crossCheck) 
-
-		elif matcher_alg == 'flann': 
-			FLANN_INDEX_LSH = 6
-			index_params= dict(algorithm = FLANN_INDEX_LSH, table_number = 6, key_size = 12, multi_probe_level = 1)
-			search_params = dict(checks=50)   # or pass empty dictionary
-
-			matcher = cv2.FlannBasedMatcher(index_params,search_params)
-
-	if distance_type == 'ratio':
-
-		matches = matcher.knnMatch(view1.descriptors, view2.descriptors, k=2)
-
-		# Need to draw only good matches, so create a mask
-		#matches_mask = [[0,0] for i in range(len(matches))]
-
-		# ratio test
-		#for i,(m,n) in enumerate(matches):
-		for m,n in matches:
-			if m.distance < 0.7*n.distance:
-				#matches_mask[i]=[1,0]
-				#closest_matches.append([m])
-				closest_matches.append(m)
-				matched_points.append([view1.keypoints[m.queryIdx].pt, view2.keypoints[m.trainIdx].pt])
-
-		#result = cv2.drawMatchesKnn(view1.image, view1.keypoints, view2.image, view2.keypoints, closest_matches, None, **draw_params)
-		#result = cv2.drawMatchesKnn(view1.image, view1.keypoints, view2.image, view2.keypoints, closest_matches, None, flags = 2)
+		view2.read_matches_file(filename)
 
 	else:
 
-		matches = matcher.match(view1.descriptors, view2.descriptors) # match() returns the best match
-		matches = sorted(matches, key = lambda x: x.distance)
-		matches = matches[:50]
+		# Brute Force Matching
 
-		for m in matches:
-			if m.distance < 170: #0.5*matches[-1].distance: 
-				closest_matches.append(m)
-				matched_points.append([view1.keypoints[m.queryIdx].pt, view2.keypoints[m.trainIdx].pt])
+		# Params: First one is normType. It specifies the distance measurement to be used. By default, it is cv.NORM_L2. It is good for SIFT, SURF etc 
+		# (cv.NORM_L1 is also there). For binary string based descriptors like ORB, BRIEF, BRISK etc, cv.NORM_HAMMING should be used, which used
+		# Hamming distance as measurement. If ORB is using WTA_K == 3 or 4, cv.NORM_HAMMING2 should be used.
+		# Second param is boolean variable, crossCheck which is false by default. If it is true, Matcher returns only those matches with value (i,j) 
+		# such that i-th descriptor in set A has j-th descriptor in set B as the best match and vice-versa. That is, the two features in both sets 
+		# should match each other.
 
-		#result = cv2.drawMatches(view1.image, view1.keypoints, view2.image, view2.keypoints, closest_matches, None, flags=2) # flags=2 hydes the features that are not matched
+		#matched_points = []
+		closest_matches = []
+		ransac_matches = []
+		crossCheck = False if distance_type == 'ratio' else True
 
-	src_pts = np.float32([ view1.keypoints[m.queryIdx].pt for m in closest_matches ])
-	dst_pts = np.float32([ view2.keypoints[m.trainIdx].pt for m in closest_matches ])
-	M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-	matches_mask = mask.ravel().tolist()
+		if feature_detection == 'sift' or feature_detection == 'surf':
 
-	draw_params = dict(matchColor = (0,255,0), singlePointColor = (255,0,0), matchesMask = matches_mask, flags = 2)
-	result = cv2.drawMatches(view1.image, view1.keypoints, view2.image, view2.keypoints, closest_matches, None, **draw_params)
+			if matcher_alg == 'brute_force':
+				matcher = cv2.BFMatcher(cv2.NORM_L2, crossCheck=crossCheck)
 
-	matched_points = np.array(matched_points)
-	view2.matched_points = matched_points[:,1]
+			elif matcher_alg == 'flann': 
+				FLANN_INDEX_KDTREE = 1
+				index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+				search_params = dict(checks=50)   # or pass empty dictionary
 
-	return result, matched_points
+				matcher = cv2.FlannBasedMatcher(index_params,search_params)
+			
+		elif feature_detection == 'orb':
+
+			if matcher_alg == 'brute_force':
+				matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=crossCheck) 
+
+			elif matcher_alg == 'flann': 
+				FLANN_INDEX_LSH = 6
+				index_params= dict(algorithm = FLANN_INDEX_LSH, table_number = 6, key_size = 12, multi_probe_level = 1)
+				search_params = dict(checks=50)   # or pass empty dictionary
+
+				matcher = cv2.FlannBasedMatcher(index_params,search_params)
+
+		if distance_type == 'ratio':
+
+			matches = matcher.knnMatch(view1.descriptors, view2.descriptors, k=2)
+
+			# Need to draw only good matches, so create a mask
+			#matches_mask = [[0,0] for i in range(len(matches))]
+
+			# ratio test
+			#for i,(m,n) in enumerate(matches):
+			for m,n in matches:
+				if m.distance < 0.7*n.distance:
+					#matches_mask[i]=[1,0]
+					#closest_matches.append([m])
+					closest_matches.append(m)
+					#matched_points.append([view1.keypoints[m.queryIdx].pt, view2.keypoints[m.trainIdx].pt])
+
+			#result = cv2.drawMatchesKnn(view1.image, view1.keypoints, view2.image, view2.keypoints, closest_matches, None, **draw_params)
+			#result = cv2.drawMatchesKnn(view1.image, view1.keypoints, view2.image, view2.keypoints, closest_matches, None, flags = 2)
+
+		else:
+
+			matches = matcher.match(view1.descriptors, view2.descriptors) # match() returns the best match
+			matches = sorted(matches, key = lambda x: x.distance)
+			#matches = matches[:50]
+
+			for m in matches:
+				if m.distance < 170: #0.5*matches[-1].distance: 
+					closest_matches.append(m)
+					#matched_points.append([view1.keypoints[m.queryIdx].pt, view2.keypoints[m.trainIdx].pt])
+
+			#result = cv2.drawMatches(view1.image, view1.keypoints, view2.image, view2.keypoints, closest_matches, None, flags=2) # flags=2 hydes the features that are not matched
+
+		src_pts = np.float32([ view1.keypoints[m.queryIdx].pt for m in closest_matches ])
+		dst_pts = np.float32([ view2.keypoints[m.trainIdx].pt for m in closest_matches ])
+		M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+		matches_mask = mask.ravel().tolist()
+
+		for idx, m in enumerate(closest_matches):
+			if mask[idx] != 0:
+				ransac_matches.append(closest_matches[idx])
+
+		view2.matches = ransac_matches
+
+		view2.write_matches_file(view1.file_name)
+
+		#matched_points = np.array(matched_points)
+		#view2.matched_points = matched_points[:,1]
+
+	#draw_params = dict(matchColor = (0,255,0), singlePointColor = (255,0,0), matchesMask = matches_mask, flags = 2)
+	#result = cv2.drawMatches(view1.image, view1.keypoints, view2.image, view2.keypoints, closest_matches, None, **draw_params)
+	result = cv2.drawMatches(view1.image, view1.keypoints, view2.image, view2.keypoints, view2.matches, None, flags = 2)
+
+	return result #, matched_points
 
 def crete_views(images_path, features_paths):
 
@@ -248,14 +298,14 @@ feature_detection = args["feature_detection"]
 distance_type = 'ratio'
 
 images_paths = get_files_paths(images_path)
-features_paths = get_files_paths(features_path) if features_path != None  else None
+features_paths = get_files_paths(features_path)
 
 views = crete_views(images_paths, features_paths)
 
-view1 = views[0]
-view2 = views[1]
+view1 = views[1]
+view2 = views[2]
 
-display_result, matches = match(view1, view2, 'brute_force', distance_type)
+display_result = match(view1, view2, 'brute_force', distance_type)
 
 #cv2.imshow("Image_{0}".format(detector), img)
 cv2.imshow("Matched result_{0}".format(feature_detection), display_result)
